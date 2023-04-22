b5d7d41ae8a85606f8c8799e1f3abd9f68c612f0
Jack McPake <s2085530@ed.ac.uk>
2023-04-22 22:50:16 +0100

    Added binary searches using merge sort and insert sort, the last one assumes the list is already sorted.

---
+++ binSearchInsert.py
@@ -0,0 +1,38 @@
+def binSearch(i, A):
+    low = 0
+    high = len(A) - 1
+    return binSearch_aux(A, int(i), low, high)
+
+def binSearch_aux(A, i, l, h):
+    while l <= h:
+        mid = (l + h) // 2
+        if A[mid] == i:
+            return True
+        elif A[mid] > i:
+            return binSearch_aux(A, i, l, mid - 1)
+        else:
+            return binSearch_aux(A, i, mid + 1, h)
+    return False
+
+def main():
+
+    import random
+    from InsertSort import InsertSort
+
+
+    Z = []
+
+    for i in range(10):
+        Z.append(random.randint(0, 99))
+
+    print("Before: " + str(Z))
+    print("After " + str(InsertSort(Z)))
+
+    target = input("Enter target: ")
+
+    if binSearch(target, Z) == True :
+        print("Target found!")
+    else:
+        print("Target not present in list.")
+
+main()
---
+++ binSearchSorted.py
@@ -0,0 +1,30 @@
+def binSearch(i, A):
+    low = 0
+    high = len(A) - 1
+    return binSearch_aux(A, int(i), low, high)
+
+def binSearch_aux(A, i, l, h):
+    while l <= h:
+        mid = (l + h) // 2
+        if A[mid] == i:
+            return True
+        elif A[mid] > i:
+            return binSearch_aux(A, i, l, mid - 1)
+        else:
+            return binSearch_aux(A, i, mid + 1, h)
+    return False
+
+def main():
+
+    Z = []
+
+    print(Z)
+
+    target = input("Enter target: ")
+
+    if binSearch(target, Z) == True :
+        print("Target found!")
+    else:
+        print("Target not present in list.")
+
+main()
